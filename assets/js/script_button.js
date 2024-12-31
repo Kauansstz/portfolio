158c23aaff917179737f6b546b57daa2434f7d92
Kauansstz <kauansantosdesouza45@gmail.com>
2024-12-30 22:49:00 -0300

    Colocado logica no botao

--- assets/js/script_button.js
+++ assets/js/script_button.js
@@ -1,18 +1,15 @@
 async function copyEmail() {
-    const email = "kauansantosdesouza45@gmail.com"; // Substitua pelo seu e-mail
+    const email = "kauansantosdesouza45@gmail.com";
     
     try {
-      // Usa a API de Clipboard para copiar o texto
       await navigator.clipboard.writeText(email);
       
-      // Mostra uma mensagem de sucesso
       const successMessage = document.getElementById("successMessage");
       successMessage.style.display = "block";
 
-      // Esconde a mensagem após 2 segundos
       setTimeout(() => {
         successMessage.style.display = "none";
-      }, 2000);
+      }, 300);
     } catch (err) {
       alert("Erro ao copiar o e-mail: " + err);
     }
